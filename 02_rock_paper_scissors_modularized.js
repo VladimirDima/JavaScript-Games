81fe1d2111a1934e16bc39728890a0d087dba92d
VladimirDima <Vladimir@Vladimirs-MacBook-Pro.local>
2018-02-08 11:54:22 +0100

    Modularized and object literal notation

--- 02_rock_paper_scissors_modularized.js
+++ 02_rock_paper_scissors_modularized.js
@@ -1,3 +1,5 @@
+// DESIGN PATTERN: MADULARIZING
+
 // The buttons
 var buttons = document.getElementsByClassName("button"); // returning an array
 for (var i = 0; i < buttons.length; i++) {
@@ -14,8 +16,51 @@ var userChoice;
 
 // main function containing the game logic
 function play(){
-	userChoice = this.id; // registering the buttons id!
+	//this in a javascript function context refer to the current owner of the event
+	userChoice = this.id; // registering the buttons id! takes the id of the button that is being clicked and stores it in user choice
 	computerInput();
 	compare();
 	display();	
+}
+
+function computerInput() {
+
+	// return a whole number between 0 and 2
+
+	switch (Math.floor(Math.random() * 3)) { 
+		case 0:
+			computerChoice = "Rock";
+			break;
+		case 1:
+			computerChoice = "Paper"
+			break;
+		case 2:
+			computerChoice = "Scissors"
+			break;
+	}
+}
+
+function compare() {
+	//game is a draw
+	if (userChoice === computerChoice) {
+		resultDisplay.innerHTML = "Draw!";
+	} else if (userChoice === 'Paper' && computerChoice === 'Rock') {
+		resultDisplay.innerHTML = "You win!";
+	} else if (userChoice === 'Scissors' && computerChoice === 'Rock') {
+		resultDisplay.innerHTML = "You loose!";
+	} else if (userChoice === 'Rock' && computerChoice === 'Scissors') {
+		resultDisplay.innerHTML = "You win!";
+	} else if (userChoice === 'Rock' && computerChoice === 'Paper') {
+		resultDisplay.innerHTML = "You loose!";
+	} else if (userChoice === 'Paper' && computerChoice === 'Scissors') {
+		resultDisplay.innerHTML = "You loose!";
+	} else if (userChoice === 'Scissors' && computerChoice === 'Paper') {
+		resultDisplay.innerHTML = "You win!";
+	}
+}
+
+function display() {
+
+	playerChoiceDisplay.innerHTML = userChoice;
+	computerChoiceDisplay.innerHTML = computerChoice;
 }
--- 02_rock_paper_scissors_object_literals.js
+++ 02_rock_paper_scissors_object_literals.js
@@ -1,22 +1,65 @@
-// a module defined using object literal notation
-// https://addyosmani.com/resources/essentialjsdesignpatterns/book/#modulepatternjavascript
 
 	var myGame = {
-		// In object literal notation, an object is described as a set of comma-separated name/value pairs enclosed in curly braces.
-		// Names inside the object may be either strings or identifiers that are followed by a colon.
- 		// object literals can contain properties and methods:
+
+		// properties
+		 playerChoiceDisplay : document.getElementById("player_choice"),
+		 computerChoiceDisplay : document.getElementById("computer_choice"),
+		 resultDisplay : document.getElementById("result"),
+		 computerChoice : '',
+		 userChoice : '',
 		
-		variableKey : variableValue,
-		
-		// a method 
-		functionkey: function () {
-			'use strict';
+		// methods 
+		computerInput : function () {
+
+			switch (Math.floor(Math.random() * 3)) { 
+				case 0:
+					this.computerChoice = "Rock";
+					break;
+				case 1:
+					this.computerChoice = "Paper"
+					break;
+				case 2:
+					this.computerChoice = "Scissors"
+					break;
+			}
 		},
 
-		///// module end /////
-	};
+		compare : function () {
 
+			if (this.userChoice === this.computerChoice) {
+				this.resultDisplay.innerHTML = "Draw!";
+			} else if (this.userChoice === 'Paper' && this.computerChoice === 'Rock') {
+				this.resultDisplay.innerHTML = "You win!";
+			} else if (this.userChoice === 'Scissors' && this.computerChoice === 'Rock') {
+				this.resultDisplay.innerHTML = "You loose!";
+			} else if (this.userChoice === 'Rock' && this.computerChoice === 'Scissors') {
+				this.resultDisplay.innerHTML = "You win!";
+			} else if (this.userChoice === 'Rock' && this.computerChoice === 'Paper') {
+				this.resultDisplay.innerHTML = "You loose!";
+			} else if (this.userChoice === 'Paper' && this.computerChoice === 'Scissors') {
+				this.resultDisplay.innerHTML = "You loose!";
+			} else if (this.userChoice === 'Scissors' && this.computerChoice === 'Paper') {
+				this.resultDisplay.innerHTML = "You win!";
+			}
+		},
+		display: function () {
+
+			this.playerChoiceDisplay.innerHTML = this.userChoice;
+			this.computerChoiceDisplay.innerHTML = this.computerChoice;
+		}
+
+		///// object end /////
+	};
+	// the buttons
+	var buttons = document.getElementsByClassName("button"); // returning an array
+		for (var i = 0; i < buttons.length; i++) {
+					buttons[i].addEventListener('click', play);
+		}
 	
+	//main logic
 	function play() {
-		myGame.functionkey();
+		myGame.userChoice = this.id;
+		myGame.computerInput();
+		myGame.compare();
+		myGame.display();
 	}
